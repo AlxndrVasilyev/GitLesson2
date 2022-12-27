da438d1e266a9af8b52373ed0410722dbafcc0d8
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-12-27 11:12:05 +0300

    finished task 21

--- Tasks/task21/Program.cs
+++ Tasks/task21/Program.cs
@@ -4,14 +4,14 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 System.Console.WriteLine ("Введите поочерёдно координаты 1-ой точки (x, y, z):");
-double x1 = double.Parse(Console.ReadLine());
-double y1 = double.Parse(Console.ReadLine());
-double z1 = double.Parse(Console.ReadLine());
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1= Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
 
 System.Console.WriteLine ("Введите поочерёдно координаты 2-ой точки: (x, y, z):");
-double x2 = double.Parse(Console.ReadLine());
-double y2 = double.Parse(Console.ReadLine());
-double z2 = double.Parse(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
 
 // записываем формулу для вычисления расстояния между двумя точками в пространстве AB = √((xb - xa)ˆ2 + (yb - ya)ˆ2 + (zb - za)ˆ2)
 // разбиваю формулу на две части
