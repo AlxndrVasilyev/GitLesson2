b427212d11be20de5d6babccfe88a78863a4aea8
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-12-27 11:40:27 +0300

    finished task23

---
+++ Tasks/task23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+System.Console.WriteLine("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    System.Console.WriteLine($"{i} в кубе = {i * i * i}");
+}
